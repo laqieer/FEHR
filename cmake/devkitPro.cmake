a8da8e787164ef775cd9c39cf41adaeff9f587ab
laqieer <laqieer@126.com>
2019-12-03 23:40:20 +0800

    Remove gbafix to build in CLion

--- cmake/devkitPro.cmake
+++ cmake/devkitPro.cmake
@@ -1,14 +1,7 @@
 # SPDX-License-Identifier: MPL-2.0
 
-find_program(GBAFIX_EXECUTABLE NAMES gbafix)
-if(NOT GBAFIX_EXECUTABLE)
-    message(SEND_ERROR "gbafix not found")
-endif()
-mark_as_advanced(GBAFIX_EXECUTABLE)
-
 function(make_gba_rom _tgt)
     add_custom_command(TARGET ${_tgt} POST_BUILD
         COMMAND ${CMAKE_OBJCOPY} --set-section-flags .rom="r,c,a" ${_tgt}
-        COMMAND ${CMAKE_OBJCOPY} ARGS -O binary ${_tgt} ${_tgt}.gba
-        COMMAND ${GBAFIX_EXECUTABLE} ARGS ${_tgt}.gba)
+        COMMAND ${CMAKE_OBJCOPY} ARGS -O binary ${_tgt} ${_tgt}.gba)
 endfunction()
