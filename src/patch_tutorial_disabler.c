e82d3886b18c828967bcfa3a21aa24f522b3e6eb
laqieer <laqieer@126.com>
2019-12-22 18:36:23 +0800

    disable tutorial

---
+++ src/patch_tutorial_disabler.c
@@ -0,0 +1,10 @@
+//
+// チュートリアルを無効にする(Tutorial Disabler)
+// チュートリアルイベントをすべて無効にします
+// disable all tutorial events (including on Easy difficulty).
+// Created by laqieer on 2019/12/22.
+//
+
+const unsigned short patch_tutorial_disabler_d6d6 = 0;
+const unsigned short patch_tutorial_disabler_d752 = 0xe004;
+const unsigned short patch_tutorial_disabler_2e3c6 = 0xe007;
