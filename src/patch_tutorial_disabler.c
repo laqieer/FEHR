beef3ea29cf3539e97af7f6d1a168876964e0370
laqieer <laqieer@126.com>
2020-02-14 07:53:58 +0800

    fix tutorial disabler patch; clean the tutorial event table
    Signed-off-by: laqieer <laqieer@126.com>

--- src/patch_tutorial_disabler.c
+++ src/patch_tutorial_disabler.c
@@ -8,3 +8,29 @@
 const unsigned short patch_tutorial_disabler_d6d6 = 0;
 const unsigned short patch_tutorial_disabler_d752 = 0xe004;
 const unsigned short patch_tutorial_disabler_2e3c6 = 0xe007;
+const unsigned int patch_tutorial_disabler_79798 = 0x47702000;
+const unsigned int patch_tutorial_disabler_797D4 = 0x47702000;
+const unsigned int patch_tutorial_disabler_7982C = 0x47702000;
+const unsigned int patch_tutorial_disabler_79894 = 0x47702000;
+const unsigned int patch_tutorial_disabler_798D4 = 0x47702000;
+
+// clean the tutorial event pointer table
+
+const int event_blank[] = {0, 0}; // an event including only ENDA
+
+const int * const tutorialEvents[] = {
+        event_blank, event_blank, event_blank, event_blank,
+        event_blank, event_blank, event_blank, event_blank,
+        event_blank, event_blank, event_blank, event_blank,
+        event_blank, event_blank, event_blank, event_blank,
+        event_blank, event_blank, event_blank, event_blank,
+        event_blank, event_blank, event_blank, event_blank,
+        event_blank, event_blank, event_blank, event_blank,
+        event_blank, event_blank, event_blank, event_blank,
+        event_blank, event_blank, event_blank, event_blank,
+        event_blank, event_blank, event_blank, event_blank,
+        event_blank, event_blank, event_blank, event_blank,
+        event_blank, event_blank, event_blank, event_blank,
+};
+
+const int ** const pTutorialEvents = tutorialEvents;
