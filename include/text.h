47b04a47e186233ac55a557ad415d910cc2fff47
laqieer <laqieer@126.com>
2020-02-13 10:38:52 +0800

    bugfix: fix error: expected '}' before string constant
    Signed-off-by: laqieer <laqieer@126.com>

--- include/text.h
+++ include/text.h
@@ -7,33 +7,33 @@
 
 // Text Control Codes (single byte)
 
-#define TCC_END '\0'
-#define TCC_NEWLINE '\x01'
-#define TCC_NEWLINE2 '\x02'
-#define TCC_PUSH_A '\x03'
-#define	TCC_PAUSE_SHORT '\x04'
-#define TCC_PAUSE '\x05'
-#define TCC_PAUSE_LONG '\x06'
-#define TCC_PAUSE_LONG_LONG '\x07'
-#define TCC_OPEN_LEFT '\x0a'
-#define TCC_OPEN_RIGHT '\x0b'
-#define TCC_OPEN_MID_LEFT '\x09'
-#define TCC_OPEN_MID_RIGHT '\x0c'
-#define TCC_OPEN_FAR_LEFT '\x08'
-#define	TCC_OPEN_FAR_RIGHT '\x0d'
-#define TCC_OPEN_FAR_FAR_LEFT '\x0e'
-#define TCC_OPEN_FAR_FAR_RIGHT '\x0f'
-#define TCC_LOAD_FACE '\x10'
-#define TCC_CLEAR_FACE '\x11'
-#define TCC_CLOSE_FAST '\x14'
-#define TCC_CLOSE_SLOW '\x15'
-#define TCC_TOGGLE_MOUTH_MOVE '\x16'
-#define TCC_TOGGLE_SMILE '\x17'
-#define TCC_YES '\x18'
-#define TCC_NO '\x19'
-#define TCC_BUY_SELL '\x1a'
-#define TCC_SEND_TO_BACK '\x1c'
-#define TCC_PAUSE_SHORT_WITH_SFX '\x1f'
+#define TCC_END "\0"
+#define TCC_NEWLINE "\x01"
+#define TCC_NEWLINE2 "\x02"
+#define TCC_PUSH_A "\x03"
+#define	TCC_PAUSE_SHORT "\x04"
+#define TCC_PAUSE "\x05"
+#define TCC_PAUSE_LONG "\x06"
+#define TCC_PAUSE_LONG_LONG "\x07"
+#define TCC_OPEN_LEFT "\x0a"
+#define TCC_OPEN_RIGHT "\x0b"
+#define TCC_OPEN_MID_LEFT "\x09"
+#define TCC_OPEN_MID_RIGHT "\x0c"
+#define TCC_OPEN_FAR_LEFT "\x08"
+#define	TCC_OPEN_FAR_RIGHT "\x0d"
+#define TCC_OPEN_FAR_FAR_LEFT "\x0e"
+#define TCC_OPEN_FAR_FAR_RIGHT "\x0f"
+#define TCC_LOAD_FACE "\x10"
+#define TCC_CLEAR_FACE "\x11"
+#define TCC_CLOSE_FAST "\x14"
+#define TCC_CLOSE_SLOW "\x15"
+#define TCC_TOGGLE_MOUTH_MOVE "\x16"
+#define TCC_TOGGLE_SMILE "\x17"
+#define TCC_YES "\x18"
+#define TCC_NO "\x19"
+#define TCC_BUY_SELL "\x1a"
+#define TCC_SEND_TO_BACK "\x1c"
+#define TCC_PAUSE_SHORT_WITH_SFX "\x1f"
 
 // Text Control Codes (double bytes)
 
