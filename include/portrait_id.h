0fd34e899565da8cc1b9d281f29a8d0f38a9c4a8
laqieer <laqieer@126.com>
2020-02-14 07:45:15 +0800

    define custom portrait id
    Signed-off-by: laqieer <laqieer@126.com>

---
+++ include/portrait_id.h
@@ -0,0 +1,36 @@
+//
+// Created by laqieer on 2020/2/12.
+//
+
+#ifndef FE7_JP_STUNNING_TRIBBLE_PORTRAIT_ID_H
+#define FE7_JP_STUNNING_TRIBBLE_PORTRAIT_ID_H
+
+#define PORTRAIT_TODO "\x1\x1"
+
+#define PORTRAIT_ALFONSE "\x1\x2"
+#define PORTRAIT_SHARENA "\x2\x2"
+#define PORTRAIT_ANNA "\x3\x2"
+#define PORTRAIT_BRUNO_MASKED "\x4\x2"
+#define PORTARIT_VERONICA "\x5\x2"
+#define PORTARIT_DEATH_KNIGHT "\x6\x2"
+#define PORTRAIT_EIR_DEATH "\x7\x2"
+#define PORTARIT_EIR PORTRAIT_EIR_DEATH
+#define PORTARIT_EIR_LIFE "\x8\x2"
+#define PORTARIT_FJORM "\x9\x2"
+#define PORTARIT_GUNNTHRA "\xA\x2"
+#define PORTARIT_HELBINDI "\xB\x2"
+#define PORTARIT_HRID "\xC\x2"
+#define PORTARIT_KRONYA "\xD\x2"
+#define PORTARIT_LAEGJARN "\xE\x2"
+#define PORTARIT_LAEVATEIN "\xF\x2"
+#define PORTARIT_LIF "\x10\x2"
+#define PORTARIT_LOKI "\x11\x2"
+#define PORTARIT_PEONY "\x12\x2"
+#define PORTARIT_SURTR "\x13\x2"
+#define PORTARIT_THRASIR "\x14\x2"
+#define PORTARIT_YLGR "\x15\x2"
+
+#define PORTRAIT_GENERIC_SODIER_BLUE "\x20\x2"
+#define PORTRAIT_GENERIC_SODIER_RED "\x21\x2"
+
+#endif //FE7_JP_STUNNING_TRIBBLE_PORTRAIT_ID_H
