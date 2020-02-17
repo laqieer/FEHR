131486aa8f17d55918aa6d5932d501d3a71cb5b3
laqieer <laqieer@126.com>
2020-02-17 16:05:11 +0800

    try to expand song tracks & notes but it has bug that makes most songs not to play

---
+++ src/patch_song_music_track_note.c
@@ -0,0 +1,9 @@
+//
+// Created by laqieer on 2020/2/17.
+//
+
+// expand song to 16 tracks 12 notes
+
+const unsigned short patch_song_music_track_note_3ea8 = 0xe010;
+const unsigned short patch_song_music_track_note_3ed0[] = {0x5641, 0x290c, 0xd002, 0x200c};
+const unsigned int patch_song_music_track_note_6ea860[] = {12, 0x3005c04, 0x3006000, 12};
