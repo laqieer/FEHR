16b6240ecd21cb5397c80148d3236a11ac013579
laqieer <laqieer@126.com>
2020-02-18 16:30:11 +0800

    export map change with map
    Signed-off-by: laqieer <laqieer@126.com>

--- tool/map/FEMapFormat.js
+++ tool/map/FEMapFormat.js
@@ -3,6 +3,15 @@ var FEMapFormat = {
     extension: "s",
 
     write: function(map, fileName) {
+        let layer;
+        let i;
+        let h;
+        let w;
+        let y0;
+        let x0;
+        let id;
+        let x;
+        let y;
         var file = new TextFile(fileName, TextFile.WriteOnly);
 
         file.writeLine("@Exported by Tiled extension: FEMapFormat.js");
@@ -21,8 +30,8 @@ var FEMapFormat = {
         file.writeLine("\t.byte ".concat(map.width, ", ", map.height));
 
         // offset array
-        for (var i = 0; i < map.layerCount; ++i) {
-            var layer = map.layerAt(i);
+        for (i = 0; i < map.layerCount; ++i) {
+            layer = map.layerAt(i);
             if (layer.isTileLayer && layer.name === "Main") {
                 for (y = 0; y < layer.height; ++y) {
                     file.write("\t.hword ");
@@ -35,7 +44,48 @@ var FEMapFormat = {
                     }
                 }
             }
+            else {
+                // map change data
+                id = layer.property("ID");
+                x0 = layer.property("X");
+                y0 = layer.property("Y");
+                w = layer.property("Width");
+                h = layer.property("Height");
+
+                file.writeLine("mapChange".concat(id, ":"));
+
+                for (y = y0; y < y0 + h; ++y) {
+                    file.write("\t.hword ");
+                    for (x = x0; x < x0 + w; ++x) {
+                        file.write(String(4 * (layer.cellAt(x, y).tileId & 1023)));
+                        if (x === x0 + w - 1)
+                            file.write("\n");
+                        else
+                            file.write(", ");
+                    }
+                }
+            }
         }
+
+        // map change config
+        file.writeLine("\t.global ".concat(label, "_change"));
+        file.writeLine(label.concat("_change", ":"));
+
+        for (i = 0; i < map.layerCount; ++i) {
+            layer = map.layerAt(i);
+            if (layer.isTileLayer && layer.name !== "Main") {
+                id = layer.property("ID");
+                x0 = layer.property("X");
+                y0 = layer.property("Y");
+                w = layer.property("Width");
+                h = layer.property("Height");
+
+                file.writeLine("\t.byte ".concat(id, ", ", x0, ", ", y0, ", ", w, ", ", h, ", 0, 0, 0"));
+                file.writeLine("\t.word mapChange".concat(id));
+            }
+        }
+
+        file.writeLine("\t.word -1, 0, 0");
 
         file.commit();
     },
