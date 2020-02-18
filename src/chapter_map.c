7a9622d318ec2857bcd36a1f62405dc312f1e026
laqieer <laqieer@126.com>
2020-02-18 16:53:20 +0800

    insert chapter 1 map & map changes
    Signed-off-by: laqieer <laqieer@126.com>

--- src/chapter_map.c
+++ src/chapter_map.c
@@ -3,11 +3,22 @@
 //
 
 #include <gba_types.h>
+#include <gba_base.h>
 
 extern const u8 map_chap_pre[];
+extern const u8 map_chap_1[];
+extern const u8 map_chap_1_change[];
 
 const u8 * const maps[] = {
         map_chap_pre,
+        map_chap_1,
 };
 
 const u8 ** const pMaps = maps;
+
+const u8 * const mapChanges[] = {
+        NULL,
+        map_chap_1_change,
+};
+
+const u8 ** const pMapChanges = mapChanges;
