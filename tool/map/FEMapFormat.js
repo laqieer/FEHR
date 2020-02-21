47992302d94c83186faa84b87fd971c977ab57c3
laqieer <laqieer@126.com>
2020-02-21 09:12:23 +0800

    simplify map without any changes
    Signed-off-by: laqieer <laqieer@126.com>

--- tool/map/FEMapFormat.js
+++ tool/map/FEMapFormat.js
@@ -29,63 +29,79 @@ var FEMapFormat = {
         // map size
         file.writeLine("\t.byte ".concat(map.width, ", ", map.height));
 
-        // offset array
-        for (i = 0; i < map.layerCount; ++i) {
-            layer = map.layerAt(i);
-            if (layer.isTileLayer && layer.name === "Main") {
-                for (y = 0; y < layer.height; ++y) {
-                    file.write("\t.hword ");
-                    for (x = 0; x < layer.width; ++x) {
-                        file.write(String(4 * (layer.cellAt(x, y).tileId & 1023)));
-                        if (x === layer.width - 1)
-                            file.write("\n");
-                        else
-                            file.write(", ");
-                    }
-                }
-            }
-            else {
-                // map change data
-                id = layer.property("ID");
-                x0 = layer.property("X");
-                y0 = layer.property("Y");
-                w = layer.property("Width");
-                h = layer.property("Height");
-
-                file.writeLine("mapChange".concat(id, ":"));
-
-                for (y = y0; y < y0 + h; ++y) {
-                    file.write("\t.hword ");
-                    for (x = x0; x < x0 + w; ++x) {
-                        file.write(String(4 * (layer.cellAt(x, y).tileId & 1023)));
-                        if (x === x0 + w - 1)
-                            file.write("\n");
-                        else
-                            file.write(", ");
-                    }
+        if(map.layerCount === 1) {
+            // for map without map changes
+            layer = map.layerAt(0);
+            for (y = 0; y < layer.height; ++y) {
+                file.write("\t.hword ");
+                for (x = 0; x < layer.width; ++x) {
+                    file.write(String(4 * (layer.cellAt(x, y).tileId & 1023)));
+                    if (x === layer.width - 1)
+                        file.write("\n");
+                    else
+                        file.write(", ");
                 }
             }
         }
+        else {
+            // offset array
+            for (i = 0; i < map.layerCount; ++i) {
+                layer = map.layerAt(i);
+                if (layer.isTileLayer && layer.name === "Main") {
+                    for (y = 0; y < layer.height; ++y) {
+                        file.write("\t.hword ");
+                        for (x = 0; x < layer.width; ++x) {
+                            file.write(String(4 * (layer.cellAt(x, y).tileId & 1023)));
+                            if (x === layer.width - 1)
+                                file.write("\n");
+                            else
+                                file.write(", ");
+                        }
+                    }
+                }
+                else {
+                    // map change data
+                    id = layer.property("ID");
+                    x0 = layer.property("X");
+                    y0 = layer.property("Y");
+                    w = layer.property("Width");
+                    h = layer.property("Height");
 
-        // map change config
-        file.writeLine("\t.global ".concat(label, "_change"));
-        file.writeLine(label.concat("_change", ":"));
+                    file.writeLine("mapChange".concat(id, ":"));
 
-        for (i = 0; i < map.layerCount; ++i) {
-            layer = map.layerAt(i);
-            if (layer.isTileLayer && layer.name !== "Main") {
-                id = layer.property("ID");
-                x0 = layer.property("X");
-                y0 = layer.property("Y");
-                w = layer.property("Width");
-                h = layer.property("Height");
-
-                file.writeLine("\t.byte ".concat(id, ", ", x0, ", ", y0, ", ", w, ", ", h, ", 0, 0, 0"));
-                file.writeLine("\t.word mapChange".concat(id));
+                    for (y = y0; y < y0 + h; ++y) {
+                        file.write("\t.hword ");
+                        for (x = x0; x < x0 + w; ++x) {
+                            file.write(String(4 * (layer.cellAt(x, y).tileId & 1023)));
+                            if (x === x0 + w - 1)
+                                file.write("\n");
+                            else
+                                file.write(", ");
+                        }
+                    }
+                }
             }
-        }
 
-        file.writeLine("\t.word -1, 0, 0");
+            // map change config
+            file.writeLine("\t.global ".concat(label, "_change"));
+            file.writeLine(label.concat("_change", ":"));
+
+            for (i = 0; i < map.layerCount; ++i) {
+                layer = map.layerAt(i);
+                if (layer.isTileLayer && layer.name !== "Main") {
+                    id = layer.property("ID");
+                    x0 = layer.property("X");
+                    y0 = layer.property("Y");
+                    w = layer.property("Width");
+                    h = layer.property("Height");
+
+                    file.writeLine("\t.byte ".concat(id, ", ", x0, ", ", y0, ", ", w, ", ", h, ", 0, 0, 0"));
+                    file.writeLine("\t.word mapChange".concat(id));
+                }
+            }
+
+            file.writeLine("\t.word -1, 0, 0");
+        }
 
         file.commit();
     },
