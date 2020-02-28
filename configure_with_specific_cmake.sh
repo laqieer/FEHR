29b91cf610c0a0e34187b130889893b2f653090c
laqieer <laqieer@126.com>
2020-02-28 16:14:43 +0800

    move cmake installation from setup to execute
    Signed-off-by: laqieer <laqieer@126.com>

--- configure_with_specific_cmake.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -z "$DEVKITPRO" ] || [ -z "$DEVKITARM" ]; then
-    echo "Environment variables not set: DEVKITPRO DEVKITARM" >&2
-    exit 1
-fi
-
-rm -rf build/
-cmake-*/bin/cmake -B build . -DCMAKE_TOOLCHAIN_FILE=toolchain-gcc.cmake "$@"
