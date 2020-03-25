d7a97368e42c66bceacd6717037b9c85c48ad992
laqieer <laqieer@126.com>
2020-03-25 14:46:07 +0800

    Disable both opening movie & class intro to solve https://github.com/laqieer/fe7-jp-stunning-tribble/issues/17.

--- src/patch_opening_movie_disabler.c
+++ src/patch_opening_movie_disabler.c
@@ -7,5 +7,12 @@
 // Skip the intro cutscene
 const unsigned short patch_opening_movie_disabler_bcfa8 = 0;
 
-// Disable replay intro, but keep class roll
+// Disable replay intro, but keep class roll (replay intro -> class roll)
 const unsigned short patch_opening_movie_disabler_12e22 = 0x2101;
+
+// Disable replay intro directly (replay intro -> null)
+const int patch_opening_movie_disabler_12e24 = 0;
+
+// Disable class roll directly (class roll -> null)
+const short patch_opening_movie_disabler_12e1a = 0;
+const short patch_opening_movie_disabler_12e1c = 0;
