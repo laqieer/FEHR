7f6e82d9f0ef36f5fafe44532b3bd8e6db3133d9
laqieer <laqieer@126.com>
2020-03-30 18:52:07 +0800

    Expand chapter amount for GetROMChapterStruct & GetChapterNameString. Fix https://github.com/laqieer/fe7-jp-stunning-tribble/issues/6.
    Signed-off-by: laqieer <laqieer@126.com>

--- src/patch_expand_chapter_amount.c
+++ src/patch_expand_chapter_amount.c
@@ -4,6 +4,8 @@
 
 #define CHAPTER_AMOUNT 0xff
 
+const unsigned char chapterStructAmount = CHAPTER_AMOUNT; // GetROMChapterStruct
 const unsigned char chapterMapAmount = CHAPTER_AMOUNT; // GetChapterMapPointer
 const unsigned char chapterMapChangeAmount = CHAPTER_AMOUNT; // GetChapterMapChangesPointer
 const unsigned char chapterEventAmount = CHAPTER_AMOUNT; // GetChapterEventDataPointer
+const unsigned char chapterNameStringAmount = CHAPTER_AMOUNT; // GetChapterNameString
