decdc0df11fb3b2fd7a49b3a2f902d32cc7ec3c6
laqieer <laqieer@126.com>
2020-04-07 22:06:11 +0800

    Create sound effect library.

---
+++ res/se/include/se.h
@@ -0,0 +1,9 @@
+extern const struct Song se_effect_anticounter;
+ extern const struct Song se_effect_cancel;
+ extern const struct Song se_effect_follow;
+ extern const struct Song se_effect_gravity;
+ extern const struct Song se_effect_panic;
+ extern const struct Song se_effect_trilemma;
+ extern const struct Song se_sys_powerdown1;
+ extern const struct Song se_sys_powerup1;
+
