51ca0d845d30a79e8bd24b0432c77c5a963c8832
laqieer <laqieer@126.com>
2020-04-13 18:16:02 +0800

    Effect of assist skill: Frightful Dream. https://github.com/laqieer/fe7-jp-stunning-tribble/issues/23.

--- src/skill.c
+++ src/skill.c
@@ -3884,14 +3884,40 @@ void assistSkillGentleDreamEffect(struct Proc* proc, struct SelectTarget* target
     m4aSongNumStart(SFX_REACTION);
     ForEachUnitInCardinalDirection(target->x, target->y, addAllyUnitBuffAttackSpeedDefenseResistanceBy3);
     ForEachUnitInCardinalDirectionExceptCenter(currentActiveUnit->positionX, currentActiveUnit->positionY, addAllyUnitBuffAttackSpeedDefenseResistanceBy3);
+    StartSoundEffect(&se_sys_powerup1);
     // Air Order has no real effect now.
     gActionData.unitActionType = UNIT_ACTION_WAIT;
 }
 
 // こわいゆめ
+int assistSkillFrightfulDreamCondition(struct Unit *targetUnit)
+{
+    return targetUnit->state & UNIT_STATE_HAS_MOVED > 0;
+}
+
+void addUnitDebuffAttackSpeedDefenseResistanceBy3(struct Unit *unit)
+{
+    addUnitDebuffPower(unit, -3);
+    addUnitDebuffSpeed(unit, -3);
+    addUnitDebuffDefense(unit, -3);
+    addUnitDebuffResistance(unit, -3);
+}
+
+void addFoeUnitDebuffAttackSpeedDefenseResistanceBy3(struct Unit *unit)
+{
+    if(isFoeUnit(unit))
+        addUnitDebuffAttackSpeedDefenseResistanceBy3(unit);
+}
+
 void assistSkillFrightfulDreamEffect(struct Proc* proc, struct SelectTarget* target)
 {
-    
+    struct Unit *targetUnit = GetUnit(target->uid);
+    giveUnitReaction(targetUnit);
+    m4aSongNumStart(SFX_REACTION);
+    ForEachUnitInCardinalDirection(target->x, target->y, addFoeUnitDebuffAttackSpeedDefenseResistanceBy3);
+    ForEachUnitInCardinalDirection(currentActiveUnit->positionX, currentActiveUnit->positionY, addFoeUnitDebuffAttackSpeedDefenseResistanceBy3);
+    StartSoundEffect(&se_sys_powerdown1);
+    gActionData.unitActionType = UNIT_ACTION_WAIT;
 }
 
 // あまいゆめ
@@ -3920,8 +3946,8 @@ void assistSkillSweetDreamsEffect(struct Proc* proc, struct SelectTarget* target
     giveUnitReaction(targetUnit);
     m4aSongNumStart(SFX_REACTION);
     addUnitBuffAttackSpeedDefenseResistanceBy3(targetUnit);
+    // I am too lazy to implement "nearest" unit, so I do "all" units here.
     ForEachUnitIn4Spaces(target->x, target->y, addFoeUnitDebuffAttackSpeedDefenseResistanceBy4);
-    // Air Order has no real effect now.
     gActionData.unitActionType = UNIT_ACTION_WAIT;
 }
 
@@ -3982,7 +4008,7 @@ const struct AssistSkill assistSkills[] = {
     {"やさしいゆめ", "このスキルは「歌う」「踊る」として扱われる。対象を行動可能\な状態にし、対象と、自分と対象の十\字方向にいる味方（自分を除く）の攻撃、速さ、守備、魔防＋３、かつ「周囲２マス以内の味方の隣接マスに移動可能\」を付与", "Gentle Dream", "Grants another action to target ally. Grants Atk/Spd/Def/Res+3 and the following status to target ally and allies in cardinal directions of unit and target (excluding unit): Unit can move to a space adjacent to any ally within 2 spaces.", assistSkillGentleDreamCondition, assistSkillGentleDreamEffect},
     {"攻撃魔防の応援＋", "対象の攻撃、魔防＋６", "Rally Atk/Res+", "Grants Atk/Res+6 to target ally for 1 turn.", NULL, assistSkillRallyAttackResistancePlusEffect},
     {"運命を変える！", "対象を自分の反対側の位置に移動させ、その後、自分を行動可能\にする、かつ自分とダブル相手の攻撃＋６、自分とダブル相手に【補助不可】を付与", "To Change Fate!", "Moves target ally to opposite side of unit and grants another action to unit. Grants Atk+6 to unit and Pair Up cohort (if any) for 1 turn and inflicts【Isolation】on unit and Pair Up cohort (if any) through their next action.", assistSkillToChangeFateCondition, assistSkillToChangeFateEffect},
-    {"こわいゆめ", "こわいゆめ", "Frightful Dream", "Grants another action to target ally. Inflicts Atk/Spd/Def/Res-3 and【Guard】on foes in cardinal directions of unit and target through their next actions.", NULL, assistSkillFrightfulDreamEffect},
+    {"こわいゆめ", "こわいゆめ", "Frightful Dream", "Grants another action to target ally. Inflicts Atk/Spd/Def/Res-3 and【Guard】on foes in cardinal directions of unit and target through their next actions.", assistSkillFrightfulDreamCondition, assistSkillFrightfulDreamEffect},
     {"あまいゆめ", "あまいゆめ", "Sweet Dreams", "Grants another action to target ally and grants Atk/Spd/Def/Res+3 to target ally for 1 turn. Inflicts Atk/Spd/Def/Res-4 on nearest foes within 4 spaces of target ally through foes' next actions.", assistSkillSweetDreamsCondition, assistSkillSweetDreamsEffect},
 };
 
