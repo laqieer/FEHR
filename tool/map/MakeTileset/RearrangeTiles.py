338816b6a7ce1e6ccccbdad3ad22aecc0ef28528
laqieer <laqieer@126.com>
2020-05-02 18:01:47 +0800

    RearrangeTiles to rearrange map tileset's width to 16px.

---
+++ tool/map/MakeTileset/RearrangeTiles.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import sys
+from PIL import Image
+
+with Image.open(sys.argv[1]) as imIn:
+    imOut = Image.new("P", (16, imIn.width * imIn.height // 16))
+    imOut.putpalette(imIn.getpalette())
+    for y in range(0, imIn.height, 16):
+        for x in range(0, imIn.width, 16):
+            imOut.paste(imIn.crop((x, y, x + 16, y + 8)), (0, imIn.width * y // 16 + x))
+            imOut.paste(imIn.crop((x, y + 8, x + 16, y + 16)), (0, imIn.width * y // 16 + x + 8))
+    imOut.save(sys.argv[2])
