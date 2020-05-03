b52d5ca3b5e531129aa1d8be82ed3c8eb6b83245
laqieer <laqieer@126.com>
2020-05-03 20:11:08 +0800

    Rework chapter 1 map.

--- src/chapter_map.c
+++ src/chapter_map.c
@@ -9,7 +9,9 @@
 
 extern const u8 map_chap_pre[];
 extern const u8 map_chap_1[];
+extern const u8 map_chap_1_new[];
 extern const u8 map_chap_1_change[];
+extern const u8 map_chap_1_new_change[];
 extern const u8 map_chap_2[];
 extern const u8 map_chap_3[];
 extern const u8 map_chap_4[];
@@ -32,6 +34,9 @@ extern const u8 map_common[];
 extern const u8 tileset_chap_pre_img[];
 extern const u8 tileset_chap_pre_cfg[];
 extern const u8 tileset_chap_pre_pal[];
+extern const u8 tileset_chap_1_img[];
+extern const u8 tileset_chap_1_cfg_lz[];
+extern const u8 tileset_chap_1_pal[];
 
 const u8 * const maps[0xff] = {
         map_chap_pre,
@@ -53,6 +58,7 @@ const u8 * const maps[0xff] = {
         map_chap_16,
         map_chap_17,
         map_common,
+        map_chap_1_new,
 };
 
 const u8 ** const pMaps = maps;
@@ -61,6 +67,7 @@ const u8 * const mapChanges[] = {
         NULL,
         map_chap_1_change,
         map_chap_9_change,
+        map_chap_1_new_change,
 };
 
 const u8 ** const pMapChanges = mapChanges;
@@ -70,6 +77,7 @@ const u8 ** const pMapChanges = mapChanges;
 const u8 * const tilesetImgs[] = {
     NULL,
     tileset_chap_pre_img,
+    tileset_chap_1_img,
 };
 
 const u8 ** const pTilesetImgs = tilesetImgs;
@@ -77,6 +85,7 @@ const u8 ** const pTilesetImgs = tilesetImgs;
 const u8 * const tilesetCfgs[] = {
     NULL,
     tileset_chap_pre_cfg,
+    tileset_chap_1_cfg_lz,
 };
 
 const u8 ** const pTilesetCfgs = tilesetCfgs;
@@ -84,6 +93,7 @@ const u8 ** const pTilesetCfgs = tilesetCfgs;
 const u8 * const tilesetPals[] = {
     NULL,
     tileset_chap_pre_pal,
+    tileset_chap_1_pal,
 };
 
 const u8 ** const pTilesetPals = tilesetPals;
