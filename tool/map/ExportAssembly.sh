9534dee79141792eed721b32bdbab46a1400f226
laqieer <laqieer@126.com>
2020-05-05 19:32:21 +0800

    Bugfix: random blank tile on map. Grit's lz77 compression is not VRAM safe. Using RLE compression instead.

--- tool/map/ExportAssembly.sh
+++ tool/map/ExportAssembly.sh
@@ -8,7 +8,7 @@ do
     grit $arg.png -gB4 -mR4 -pn160 -ftb
     cat $arg.map.bin $arg.terrain.bin > $arg.cfg.bin
     gbalzss e $arg.cfg.bin $arg.cfg
-    grit $arg.png -gB4 -mR4 -pn160 -gzl
+    grit $arg.png -gB4 -mR4 -pn160 -gzr
     sed '/Map/,/align/d' $arg.s > ../../../src/res/map/map/$arg.s
     bin2s $arg.cfg >> ../../../src/res/map/map/$arg.s
 done
