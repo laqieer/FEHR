7df4dfa881cd931ecdba74c3e99a1c7a7e21c3dc
laqieer <laqieer@126.com>
2020-05-19 09:01:57 +0800

    Chapter 45.

--- src/chapter_event.c
+++ src/chapter_event.c
@@ -50,6 +50,7 @@ extern const u8 * const event_chap_41[];
 extern const u8 * const event_chap_42[];
 extern const u8 * const event_chap_43[];
 extern const u8 * const event_chap_44[];
+extern const u8 * const event_chap_45[];
 
 const u8 ** const events[0xff] = {
         NULL,
@@ -98,6 +99,7 @@ const u8 ** const events[0xff] = {
         event_chap_42,
         event_chap_43,
         event_chap_44,
+        event_chap_45,
 };
 
 const u8 *** const pEvents = events;
--- src/chapter_map.c
+++ src/chapter_map.c
@@ -180,6 +180,9 @@ extern const u8 S1001Pal[];
 extern const u8 S1002Tiles[];
 extern const u8 S1002_cfg[];
 extern const u8 S1002Pal[];
+extern const u8 S1003Tiles[];
+extern const u8 S1003_cfg[];
+extern const u8 S1003Pal[];
 
 const u8 * const maps[0xff] = {
         map_chap_pre,
@@ -278,6 +281,7 @@ const u8 * const tilesetImgs[] = {
     S0905Tiles,
     S1001Tiles,
     S1002Tiles,
+    S1003Tiles,
 };
 
 const u8 ** const pTilesetImgs = tilesetImgs;
@@ -329,6 +333,7 @@ const u8 * const tilesetCfgs[] = {
     S0905_cfg,
     S1001_cfg,
     S1002_cfg,
+    S1003_cfg,
 };
 
 const u8 ** const pTilesetCfgs = tilesetCfgs;
@@ -380,6 +385,7 @@ const u8 * const tilesetPals[] = {
     S0905Pal,
     S1001Pal,
     S1002Pal,
+    S1003Pal,
 };
 
 const u8 ** const pTilesetPals = tilesetPals;
