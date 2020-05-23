bcb25c9f407062218deb44b29e26653b7d4198a2
laqieer <laqieer@126.com>
2020-05-23 12:08:30 +0800

    Enable 3 lords to use Heaven Seal to promote.

---
+++ src/promotion.c
@@ -0,0 +1,8 @@
+// Job promotion.
+
+#include <gba_types.h>
+
+const u8 jobListLord[] = {1, 2, 3, 0};
+
+const u8 * const pJobListLordEM = jobListLord;
+const u8 * const pJobListLordHM = jobListLord;
