eb835fcb6aafa5938fb3157f67430c25c2a354ac
laqieer <laqieer@126.com>
2020-06-11 21:08:24 +0800

    Loki's portrait.

--- src/portrait.c
+++ src/portrait.c
@@ -28,6 +28,8 @@
 #include "portrait_Fjorm_bg_tileset.h"
 #include "portrait_Surtr_mini.h"
 #include "portrait_Surtr_tileset.h"
+#include "portrait_Loki_mini.h"
+#include "portrait_Loki_tileset.h"
 #include "portrait_Lord_classcard.h"
 #include "classcard_MageKnight.h"
 
@@ -74,6 +76,21 @@ const u16 portrait_Surtr_obj[] = {
     OBJ_Y(-16) | ATTR0_SQUARE, OBJ_X(32 - 48) | OBJ_SIZE(2), OBJ_CHAR(0),
     OBJ_Y(0) | ATTR0_WIDE, OBJ_X(0 - 48) | OBJ_SIZE(2), OBJ_CHAR(4),
     OBJ_Y(0) | ATTR0_WIDE, OBJ_X(64 - 48) | OBJ_SIZE(2), OBJ_CHAR(4 + 32 * 2),
+    OBJ_Y(32 - 16) | ATTR0_WIDE, OBJ_X(0 - 48) | OBJ_SIZE(3), OBJ_CHAR(8),
+    OBJ_Y(32 - 16) | ATTR0_SQUARE, OBJ_X(64 - 48) | OBJ_SIZE(2), OBJ_CHAR(16),
+    OBJ_Y(64 - 16) | ATTR0_WIDE, OBJ_X(0 - 48) | OBJ_SIZE(3), OBJ_CHAR(20),
+    OBJ_Y(64 - 16) | ATTR0_SQUARE, OBJ_X(64 - 48) | OBJ_SIZE(2), OBJ_CHAR(28),
+};
+
+const u16 portrait_Loki_obj[] = {
+    5,
+    OBJ_Y(-16) | ATTR0_WIDE, OBJ_X(-(24 - 48) - 64) | OBJ_SIZE(3) | OBJ_HFLIP, OBJ_CHAR(0),
+    OBJ_Y(32 - 16) | ATTR0_WIDE, OBJ_X(-(0 - 48) - 64) | OBJ_SIZE(3) | OBJ_HFLIP, OBJ_CHAR(8),
+    OBJ_Y(32 - 16) | ATTR0_SQUARE, OBJ_X(-(64 - 48) - 32) | OBJ_SIZE(2) | OBJ_HFLIP, OBJ_CHAR(16),
+    OBJ_Y(64 - 16) | ATTR0_WIDE, OBJ_X(-(0 - 48) - 64) | OBJ_SIZE(3) | OBJ_HFLIP, OBJ_CHAR(20),
+    OBJ_Y(64 - 16) | ATTR0_SQUARE, OBJ_X(-(64 - 48) - 32) | OBJ_SIZE(2) | OBJ_HFLIP, OBJ_CHAR(28),
+    5,
+    OBJ_Y(-16) | ATTR0_WIDE, OBJ_X(24 - 48) | OBJ_SIZE(3), OBJ_CHAR(0),
     OBJ_Y(32 - 16) | ATTR0_WIDE, OBJ_X(0 - 48) | OBJ_SIZE(3), OBJ_CHAR(8),
     OBJ_Y(32 - 16) | ATTR0_SQUARE, OBJ_X(64 - 48) | OBJ_SIZE(2), OBJ_CHAR(16),
     OBJ_Y(64 - 16) | ATTR0_WIDE, OBJ_X(0 - 48) | OBJ_SIZE(3), OBJ_CHAR(20),
@@ -161,6 +178,19 @@ const u16 portrait_Surtr_bg[] = {
     6, 6, 6, 0, 1, 2, 3, 6, 6, 6,
 };
 
+const u16 portrait_Loki_bg[] = {
+    0x809,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+    9 + 32 * 3, 10 + 32 * 3, 11 + 32 * 3, 12 + 32 * 3, 13 + 32 * 3, 14 + 32 * 3, 15 + 32 * 3, 16 + 32 * 3, 17 + 32 * 3, 18 + 32 * 3,
+    9 + 32 * 2, 10 + 32 * 2, 11 + 32 * 2, 12 + 32 * 2, 13 + 32 * 2, 14 + 32 * 2, 15 + 32 * 2, 16 + 32 * 2, 17 + 32 * 2, 18 + 32 * 2,
+    9 + 32, 10 + 32, 11 + 32, 12 + 32, 13 + 32, 14 + 32, 15 + 32, 16 + 32, 17 + 32, 18 + 32,
+    9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+    0, 0, 0 + 32 * 3, 1 + 32 * 3, 2 + 32 * 3, 3 + 32 * 3, 4 + 32 * 3, 5 + 32 * 3, 6 + 32 * 3, 7 + 32 * 3,
+    0, 0, 0 + 32 * 2, 1 + 32 * 2, 2 + 32 * 2, 3 + 32 * 2, 4 + 32 * 2, 5 + 32 * 2, 6 + 32 * 2, 7 + 32 * 2,
+    0, 0, 0 + 32, 1 + 32, 2 + 32, 3 + 32, 4 + 32, 5 + 32, 6 + 32, 7 + 32,
+    0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+};
+
 const u16 portrait_Bruno_bg[] = {
         0x809,
         20 + 32, 21 + 32, 22 + 32, 23 + 32, 24 + 32, 25 + 32, 26 + 32, 27 + 32, 28 + 32, 29 + 32,
@@ -206,6 +236,7 @@ DEFINE_PORTRAIT_SPECIAL_WITHOUT_MASK(Bruno)
 DEFINE_PORTRAIT_SPECIAL_WITHOUT_MASK(Xander)
 DEFINE_PORTRAIT_SPECIAL_WITHOUT_MASK(Veronica)
 DEFINE_PORTRAIT_SPECIAL_WITHOUT_MASK(Surtr)
+DEFINE_PORTRAIT_SPECIAL_WITHOUT_MASK(Loki)
 
 const Portrait portraitFjorm = {
     portrait_Fjorm_tilesetTiles,
@@ -237,6 +268,7 @@ const Portrait* const portraits[] = {
     [0x104] = &portraitBruno,
     [0x105] = &portraitVeronica,
     [0x109] = &portraitFjorm,
+    [0x111] = &portraitLoki,
     [0x113] = &portraitSurtr,
     [0x116] = &portraitXander,
     [0x130] = &classcardLord,
