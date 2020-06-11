291611a7d37252f325d95fc0b1201fe888f795fa
laqieer <laqieer@126.com>
2020-06-12 01:01:15 +0800

    Cerise's portrait

--- src/portrait.c
+++ src/portrait.c
@@ -32,6 +32,8 @@
 #include "portrait_Loki_tileset.h"
 #include "portrait_Laevatain_mini.h"
 #include "portrait_Laevatain_tileset.h"
+#include "portrait_Cerise_mini.h"
+#include "portrait_Cerise_tileset.h"
 #include "portrait_Lord_classcard.h"
 #include "classcard_MageKnight.h"
 
@@ -63,6 +65,29 @@ const u16 portrait_Fjorm_obj[] = {
     OBJ_Y(-16 + 64) | ATTR0_WIDE, OBJ_X(0 - 56) | OBJ_SIZE(3), OBJ_CHAR(18),
     OBJ_Y(-16 + 64) | ATTR0_SQUARE, OBJ_X(64 - 56) | OBJ_SIZE(2), OBJ_CHAR(26),
     OBJ_Y(-16 + 64) | ATTR0_TALL, OBJ_X(96 - 56) | OBJ_SIZE(2), OBJ_CHAR(30),
+};
+
+const u16 portrait_Cerise_obj[] = {
+    9,
+    OBJ_Y(-16) | ATTR0_WIDE, OBJ_X(-(16 - 52) - 64) | OBJ_SIZE(3) | OBJ_HFLIP, OBJ_CHAR(0),
+    OBJ_Y(-16) | ATTR0_TALL, OBJ_X(-(80 - 52) - 8) | OBJ_SIZE(1) | OBJ_HFLIP, OBJ_CHAR(8),
+    OBJ_Y(32 - 16) | ATTR0_WIDE, OBJ_X(-(16 - 52) - 64) | OBJ_SIZE(3) | OBJ_HFLIP, OBJ_CHAR(9),
+    OBJ_Y(32 - 16) | ATTR0_TALL, OBJ_X(-(80 - 52) - 8) | OBJ_SIZE(1) | OBJ_HFLIP, OBJ_CHAR(17),
+    OBJ_Y(48 - 16) | ATTR0_TALL, OBJ_X(-(88 - 52) - 8) | OBJ_SIZE(0) | OBJ_HFLIP, OBJ_CHAR(18 + 32 * 2),
+    OBJ_Y(48 - 16) | ATTR0_TALL, OBJ_X(-(8 - 52) - 8) | OBJ_SIZE(0) | OBJ_HFLIP, OBJ_CHAR(18),
+    OBJ_Y(64 - 16) | ATTR0_WIDE, OBJ_X(-(0 - 52) - 64) | OBJ_SIZE(3) | OBJ_HFLIP, OBJ_CHAR(19),
+    OBJ_Y(64 - 16) | ATTR0_SQUARE, OBJ_X(-(64 - 52) - 32) | OBJ_SIZE(2) | OBJ_HFLIP, OBJ_CHAR(27),
+    OBJ_Y(64 - 16) | ATTR0_TALL, OBJ_X(-(96 - 52) - 8) | OBJ_SIZE(1) | OBJ_HFLIP, OBJ_CHAR(31),
+    9,
+    OBJ_Y(-16) | ATTR0_WIDE, OBJ_X(16 - 52) | OBJ_SIZE(3), OBJ_CHAR(0),
+    OBJ_Y(-16) | ATTR0_TALL, OBJ_X(80 - 52) | OBJ_SIZE(1), OBJ_CHAR(8),
+    OBJ_Y(32 - 16) | ATTR0_WIDE, OBJ_X(16 - 52) | OBJ_SIZE(3), OBJ_CHAR(9),
+    OBJ_Y(32 - 16) | ATTR0_TALL, OBJ_X(80 - 52) | OBJ_SIZE(1), OBJ_CHAR(17),
+    OBJ_Y(48 - 16) | ATTR0_TALL, OBJ_X(88 - 52) | OBJ_SIZE(0), OBJ_CHAR(18 + 32 * 2),
+    OBJ_Y(48 - 16) | ATTR0_TALL, OBJ_X(8 - 52) | OBJ_SIZE(0), OBJ_CHAR(18),
+    OBJ_Y(64 - 16) | ATTR0_WIDE, OBJ_X(0 - 52) | OBJ_SIZE(3), OBJ_CHAR(19),
+    OBJ_Y(64 - 16) | ATTR0_SQUARE, OBJ_X(64 - 52) | OBJ_SIZE(2), OBJ_CHAR(27),
+    OBJ_Y(64 - 16) | ATTR0_TALL, OBJ_X(96 - 52) | OBJ_SIZE(1), OBJ_CHAR(31),
 };
 
 const u16 portrait_Surtr_obj[] = {
@@ -206,6 +231,19 @@ const u16 portrait_Laevatain_bg[] = {
     8, 0, 1, 2, 3, 4, 5, 6, 7,
 };
 
+const u16 portrait_Cerise_bg[] = {
+    0x809,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+    9 + 32 * 3, 10 + 32 * 3, 11 + 32 * 3, 12 + 32 * 3, 13 + 32 * 3, 14 + 32 * 3, 15 + 32 * 3, 16 + 32 * 3, 17 + 32 * 3, 18 + 32 * 3,
+    9 + 32 * 2, 10 + 32 * 2, 11 + 32 * 2, 12 + 32 * 2, 13 + 32 * 2, 14 + 32 * 2, 15 + 32 * 2, 16 + 32 * 2, 17 + 32 * 2, 18 + 32 * 2,
+    9 + 32, 10 + 32, 11 + 32, 12 + 32, 13 + 32, 14 + 32, 15 + 32, 16 + 32, 17 + 32, 0,
+    9, 10, 11, 12, 13, 14, 15, 16, 17, 0,
+    0 + 32 * 3, 1 + 32 * 3, 2 + 32 * 3, 3 + 32 * 3, 4 + 32 * 3, 5 + 32 * 3, 6 + 32 * 3, 7 + 32 * 3, 8 + 32 * 3, 0,
+    0 + 32 * 2, 1 + 32 * 2, 2 + 32 * 2, 3 + 32 * 2, 4 + 32 * 2, 5 + 32 * 2, 6 + 32 * 2, 7 + 32 * 2, 8 + 32 * 2, 0,
+    0 + 32, 1 + 32, 2 + 32, 3 + 32, 4 + 32, 5 + 32, 6 + 32, 7 + 32, 8 + 32, 0,
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 0,
+};
+
 const u16 portrait_Bruno_bg[] = {
         0x809,
         20 + 32, 21 + 32, 22 + 32, 23 + 32, 24 + 32, 25 + 32, 26 + 32, 27 + 32, 28 + 32, 29 + 32,
@@ -252,6 +290,7 @@ DEFINE_PORTRAIT_SPECIAL_WITHOUT_MASK(Xander)
 DEFINE_PORTRAIT_SPECIAL_WITHOUT_MASK(Veronica)
 DEFINE_PORTRAIT_SPECIAL_WITHOUT_MASK(Surtr)
 DEFINE_PORTRAIT_SPECIAL_WITHOUT_MASK(Loki)
+DEFINE_PORTRAIT_SPECIAL_WITHOUT_MASK(Cerise)
 
 const Portrait portraitFjorm = {
     portrait_Fjorm_tilesetTiles,
@@ -293,6 +332,7 @@ const Portrait* const portraits[] = {
     [0x104] = &portraitBruno,
     [0x105] = &portraitVeronica,
     [0x109] = &portraitFjorm,
+    [0x10A] = &portraitCerise,
     [0x10F] = &portraitLaevatain,
     [0x111] = &portraitLoki,
     [0x113] = &portraitSurtr,
