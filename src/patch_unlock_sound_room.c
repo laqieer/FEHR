117f62da2996c834ce4fca71a263f80f922a4924
laqieer <laqieer@126.com>
2020-06-30 15:10:14 +0800

    Bugfix: fix sound room unlock all patch. Entries beyond 140 are unlocked now.

--- src/patch_unlock_sound_room.c
+++ src/patch_unlock_sound_room.c
@@ -2,6 +2,11 @@
 // Created by laqieer on 2020/2/17.
 //
 
-const short patch_unlock_sound_room[] = {0x2800, 0, 0x1c20, 0x0220};
+#include "proc.h"
 
-const char patch_unlock_sound_room_unlock_all = -1;
+const short patch_unlock_sound_room_unlock_all = 0;
+
+int checkIfSoundRoomEntryUnlocked(struct Proc *proc, int SoundRoomEntryId)
+{
+    return 1;
+}
