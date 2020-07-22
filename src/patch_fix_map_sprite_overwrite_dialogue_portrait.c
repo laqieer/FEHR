1b3939d4d845efe69cf88261ccd60a31cc4a85ae
laqieer <laqieer@126.com>
2020-07-23 07:42:12 +0800

    Bugfix: moving map sprite overwrites help text's tiles.

--- src/patch_fix_map_sprite_overwrite_dialogue_portrait.c
+++ src/patch_fix_map_sprite_overwrite_dialogue_portrait.c
@@ -1,3 +1,5 @@
-// Fix vanilla bug: 4th portrait's tileset in dialogue is overwritten by moving map sprite. Method: Move moving map sprite's tiles from 0x6017000 to 0x6013000.
+// Fix vanilla bug: 4th portrait's tileset in dialogue is overwritten by moving map sprite. Method: Move moving map sprite's tiles from 0x6017000 to 0x6010340.
 
-const unsigned char movingSpriteTileBase = 0x60;
+// 0x4a << 8 - 6 = 0x49a
+const unsigned char movingSpriteTileBaseFactor1 = 0x4a;
+const unsigned short movingSpriteTileBaseFactor2 = 0x1f8a; // sub r2, r1, #6
