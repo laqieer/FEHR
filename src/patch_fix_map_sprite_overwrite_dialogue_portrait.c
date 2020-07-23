d10a401f1a847ee3434ab22034dc6e0a69f3019d
laqieer <laqieer@126.com>
2020-07-23 19:01:14 +0800

    Bugfix: moving sprite in map battle animation.

--- src/patch_fix_map_sprite_overwrite_dialogue_portrait.c
+++ src/patch_fix_map_sprite_overwrite_dialogue_portrait.c
@@ -1,5 +1,7 @@
-// Fix vanilla bug: 4th portrait's tileset in dialogue is overwritten by moving map sprite. Method: Move moving map sprite's tiles from 0x6017000 to 0x6010340.
+// Fix vanilla bug: 4th portrait's tileset in dialogue is overwritten by moving map sprite. Method: Move moving map sprite's tiles from 0x6017000 to 0x6010240.
 
-// 0x4a << 8 - 6 = 0x49a
-const unsigned char movingSpriteTileBaseFactor1 = 0x4a;
-const unsigned short movingSpriteTileBaseFactor2 = 0x1f8a; // sub r2, r1, #6
+// 0x49 << 8 + 2 = 0x492
+const unsigned char movingSpriteTileBaseFactor1 = 0x49;
+const unsigned short movingSpriteTileBaseFactor2 = 0x1c8a; // add r2, r1, #2
+const short movingSpriteTileBaseOffsets[] = {0, 4 * 2, 4, 4 * 3};
+
