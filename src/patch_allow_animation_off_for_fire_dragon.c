7f9d00909a2125f4aa116245ed0342cdd1e1e901
laqieer <laqieer@126.com>
2020-07-27 08:46:51 +0800

    Allow to set animation off for fire dragon.

---
+++ src/patch_allow_animation_off_for_fire_dragon.c
@@ -0,0 +1,4 @@
+// Allow to turn off battle animation for fire dragon.
+
+const unsigned short disableFireDragonDetectionInBattleDataInitialization1 = 0;
+const unsigned short disableFireDragonDetectionInBattleDataInitialization2 = 0;
