c891d61209a9e7717544f6be2a11963f66dd649d
laqieer <laqieer@126.com>
2022-06-05 11:51:23 +0800

    Remake Anna's portrait.

--- src/portrait.c
+++ src/portrait.c
@@ -12,8 +12,8 @@
 
 #include "portrait_Alfons_tileset.h"
 #include "portrait_Alfons_mini.h"
-#include "portrait_Anna_commander_tileset.h"
-#include "portrait_Anna_commander_mini.h"
+#include "portrait_Anna_tileset.h"
+#include "portrait_Anna_mini.h"
 #include "portrait_Sharena_princess_tileset.h"
 #include "portrait_Sharena_princess_mini.h"
 #include "portrait_Bruno_mini.h"
@@ -1555,8 +1555,40 @@ const u16 portrait_Alfons_bg[] = {
         0, 0, TILE_ID(1, 0), TILE_ID(2, 0), TILE_ID(3, 0), TILE_ID(4, 0), TILE_ID(5, 0), TILE_ID(6, 0), 0, 0,
 };
 
+#define portrait_Anna_width 96
+#define portrait_Anna_height 96
+const u16 portrait_Anna_obj[] = {
+    6,
+    PORTRAIT_BLOCK(Anna, 8, 32, 16, 0, 0, 0),
+    PORTRAIT_BLOCK(Anna, 64, 32, 24, 0, 1, 0),
+    PORTRAIT_BLOCK(Anna, 64, 32, 0, 32, 9, 0),
+    PORTRAIT_BLOCK(Anna, 32, 32, 56, 32, 16, 0),
+    PORTRAIT_BLOCK(Anna, 64, 32, 0, 64, 20, 0),
+    PORTRAIT_BLOCK(Anna, 32, 32, 64, 64, 28, 0),
+    6,
+    PORTRAIT_BLOCK_R(Anna, 8, 32, 16, 0, 0, 0),
+    PORTRAIT_BLOCK_R(Anna, 64, 32, 24, 0, 1, 0),
+    PORTRAIT_BLOCK_R(Anna, 64, 32, 0, 32, 9, 0),
+    PORTRAIT_BLOCK_R(Anna, 32, 32, 56, 32, 16, 0),
+    PORTRAIT_BLOCK_R(Anna, 64, 32, 0, 64, 20, 0),
+    PORTRAIT_BLOCK_R(Anna, 32, 32, 64, 64, 28, 0),
+};
+
+const u16 portrait_Anna_bg[] = {
+        0x809,
+        TILE_ID(21, 0), TILE_ID(22, 0), TILE_ID(23, 0), TILE_ID(24, 0), TILE_ID(25, 0), TILE_ID(26, 0), TILE_ID(27, 0), TILE_ID(28, 0), TILE_ID(29, 0), TILE_ID(30, 0),
+        TILE_ID(10, 3), TILE_ID(11, 3), TILE_ID(12, 3), TILE_ID(13, 3), TILE_ID(14, 3), TILE_ID(15, 3), TILE_ID(16, 3), TILE_ID(17, 3), TILE_ID(18, 3), TILE_ID(19, 3),
+        TILE_ID(10, 2), TILE_ID(11, 2), TILE_ID(12, 2), TILE_ID(13, 2), TILE_ID(14, 2), TILE_ID(15, 2), TILE_ID(16, 2), TILE_ID(17, 2), TILE_ID(18, 2), TILE_ID(19, 2),
+        TILE_ID(10, 1), TILE_ID(11, 1), TILE_ID(12, 1), TILE_ID(13, 1), TILE_ID(14, 1), TILE_ID(15, 1), TILE_ID(16, 1), TILE_ID(17, 1), TILE_ID(18, 1), TILE_ID(19, 1),
+        TILE_ID(10, 0), TILE_ID(11, 0), TILE_ID(12, 0), TILE_ID(13, 0), TILE_ID(14, 0), TILE_ID(15, 0), TILE_ID(16, 0), TILE_ID(17, 0), TILE_ID(18, 0), TILE_ID(19, 0),
+        0, TILE_ID(0, 3), TILE_ID(1, 3), TILE_ID(2, 3), TILE_ID(3, 3), TILE_ID(4, 3), TILE_ID(5, 3), TILE_ID(6, 3), TILE_ID(7, 3), TILE_ID(8, 3),
+        0, TILE_ID(0, 2), TILE_ID(1, 2), TILE_ID(2, 2), TILE_ID(3, 2), TILE_ID(4, 2), TILE_ID(5, 2), TILE_ID(6, 2), TILE_ID(7, 2), TILE_ID(8, 2),
+        0, TILE_ID(0, 1), TILE_ID(1, 1), TILE_ID(2, 1), TILE_ID(3, 1), TILE_ID(4, 1), TILE_ID(5, 1), TILE_ID(6, 1), TILE_ID(7, 1), TILE_ID(8, 1),
+        0, TILE_ID(0, 0), TILE_ID(1, 0), TILE_ID(2, 0), TILE_ID(3, 0), TILE_ID(4, 0), TILE_ID(5, 0), TILE_ID(6, 0), TILE_ID(7, 0), TILE_ID(8, 0),
+};
+
 DEFINE_PORTRAIT_SPECIAL_WITHOUT_MASK(Alfons)
-DEFINE_PORTRAIT(Anna_commander)
+DEFINE_PORTRAIT_SPECIAL_WITHOUT_MASK(Anna)
 DEFINE_PORTRAIT(Sharena_princess)
 DEFINE_PORTRAIT_SPECIAL_WITHOUT_MASK(Bruno)
 DEFINE_PORTRAIT_SPECIAL_WITHOUT_MASK(Xander)
@@ -1641,7 +1673,7 @@ DEFINE_CLASSCARD_NEW(Wolf)
 const Portrait* const portraits[] = {
     [0x101] = &portraitAlfons,
     [0x102] = &portraitSharena_princess,
-    [0x103] = &portraitAnna_commander,
+    [0x103] = &portraitAnna,
     [0x104] = &portraitBruno,
     [0x105] = &portraitVeronica,
     [0x107] = &portraitEir,
