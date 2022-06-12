e7d5aa2bdb4696c1db9459c09672749a3b14f81a
laqieer <laqieer@126.com>
2022-06-12 16:11:49 +0800

    Female Avatar portrait 1.

--- src/portrait.c
+++ src/portrait.c
@@ -98,6 +98,8 @@
 #include "portrait_GenericSoldierBlue_tileset.h"
 #include "portrait_GenericSoldierRed.h"
 #include "portrait_GenericSoldierGreen.h"
+#include "portrait_Avatar_F_1_mini.h"
+#include "portrait_Avatar_F_1_tileset.h"
 #include "portrait_Lord_classcard.h"
 #include "classcard_MageKnight.h"
 #include "classcard_Halberdier.h"
@@ -1706,6 +1708,37 @@ const u16 portrait_Marks_bg[] = {
         12, 12, TILE_ID(0, 0), TILE_ID(1, 0), TILE_ID(2, 0), TILE_ID(3, 0), 12, 12, 12, 12,
 };
 
+#define portrait_Avatar_F_1_width 96
+#define portrait_Avatar_F_1_height 96
+#define portrait_Avatar_F_1_block_num 5
+const u16 portrait_Avatar_F_1_obj[] = {
+    portrait_Avatar_F_1_block_num,
+    PORTRAIT_BLOCK(Avatar_F_1, 64, 32, 16, 0, 0, 0),
+    PORTRAIT_BLOCK(Avatar_F_1, 64, 32, 8, 32, 8, 0),
+    PORTRAIT_BLOCK(Avatar_F_1, 16, 32, 72, 32, 16, 0),
+    PORTRAIT_BLOCK(Avatar_F_1, 64, 32, 0, 64, 18, 0),
+    PORTRAIT_BLOCK(Avatar_F_1, 32, 32, 64, 64, 26, 0),
+    portrait_Avatar_F_1_block_num,
+    PORTRAIT_BLOCK_R(Avatar_F_1, 64, 32, 16, 0, 0, 0),
+    PORTRAIT_BLOCK_R(Avatar_F_1, 64, 32, 8, 32, 8, 0),
+    PORTRAIT_BLOCK_R(Avatar_F_1, 16, 32, 72, 32, 16, 0),
+    PORTRAIT_BLOCK_R(Avatar_F_1, 64, 32, 0, 64, 18, 0),
+    PORTRAIT_BLOCK_R(Avatar_F_1, 32, 32, 64, 64, 26, 0),
+};
+
+const u16 portrait_Avatar_F_1_bg[] = {
+        0x809,
+        TILE_ID(19, 0), TILE_ID(20, 0), TILE_ID(21, 0), TILE_ID(22, 0), TILE_ID(23, 0), TILE_ID(24, 0), TILE_ID(25, 0), TILE_ID(26, 0), TILE_ID(27, 0), TILE_ID(28, 0),
+        TILE_ID(8, 3), TILE_ID(9, 3), TILE_ID(10, 3), TILE_ID(11, 3), TILE_ID(12, 3), TILE_ID(13, 3), TILE_ID(14, 3), TILE_ID(15, 3), TILE_ID(16, 3), TILE_ID(17, 3),
+        TILE_ID(8, 2), TILE_ID(9, 2), TILE_ID(10, 2), TILE_ID(11, 2), TILE_ID(12, 2), TILE_ID(13, 2), TILE_ID(14, 2), TILE_ID(15, 2), TILE_ID(16, 2), TILE_ID(17, 2),
+        TILE_ID(8, 1), TILE_ID(9, 1), TILE_ID(10, 1), TILE_ID(11, 1), TILE_ID(12, 1), TILE_ID(13, 1), TILE_ID(14, 1), TILE_ID(15, 1), TILE_ID(16, 1), TILE_ID(17, 1),
+        TILE_ID(8, 0), TILE_ID(9, 0), TILE_ID(10, 0), TILE_ID(11, 0), TILE_ID(12, 0), TILE_ID(13, 0), TILE_ID(14, 0), TILE_ID(15, 0), TILE_ID(16, 0), TILE_ID(17, 0),
+        0, TILE_ID(0, 3), TILE_ID(1, 3),  TILE_ID(2, 3),  TILE_ID(3, 3),  TILE_ID(4, 3),  TILE_ID(5, 3),  TILE_ID(6, 3),  TILE_ID(7, 3), 0,
+        0, TILE_ID(0, 2), TILE_ID(1, 2),  TILE_ID(2, 2),  TILE_ID(3, 2),  TILE_ID(4, 2),  TILE_ID(5, 2),  TILE_ID(6, 2),  TILE_ID(7, 2), 0,
+        0, TILE_ID(0, 1), TILE_ID(1, 1),  TILE_ID(2, 1),  TILE_ID(3, 1),  TILE_ID(4, 1),  TILE_ID(5, 1),  TILE_ID(6, 1),  TILE_ID(7, 1), 0,
+        0, TILE_ID(0, 0), TILE_ID(1, 0),  TILE_ID(2, 0),  TILE_ID(3, 0),  TILE_ID(4, 0),  TILE_ID(5, 0),  TILE_ID(6, 0),  TILE_ID(7, 0), 0,
+};
+
 DEFINE_PORTRAIT_SPECIAL_WITHOUT_MASK(Alfons)
 DEFINE_PORTRAIT_SPECIAL_WITHOUT_MASK(Anna)
 DEFINE_PORTRAIT_SPECIAL_WITHOUT_MASK(Sharon)
@@ -1748,6 +1781,7 @@ DEFINE_PORTRAIT_SPECIAL_WITHOUT_MASK(Elm)
 DEFINE_PORTRAIT_SPECIAL_WITHOUT_MASK(GenericSoldierBlue)
 DEFINE_PORTRAIT_SPECIAL_WITHOUT_MASK_RECOLOR(GenericSoldierRed, GenericSoldierBlue)
 DEFINE_PORTRAIT_SPECIAL_WITHOUT_MASK_RECOLOR(GenericSoldierGreen, GenericSoldierBlue)
+DEFINE_PORTRAIT_SPECIAL_WITHOUT_MASK(Avatar_F_1)
 
 const Portrait portraitFjorm = {
     portrait_Fjorm_tilesetTiles,
@@ -1844,6 +1878,7 @@ const Portrait* const portraits[] = {
     [0x140] = &portraitFjormBG,
     [PORTRAIT_ID_DAGR_BG] = &portraitDagrBG,
     [PORTRAIT_ID_NOTT_BG] = &portraitNottBG,
+    [PORTRAIT_ID_AVATAR_F_1] = &portraitAvatar_F_1,
 };
 
 extern const Portrait portraitsOrig[];
