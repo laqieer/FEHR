laqieer/FEHR

commit fa27da8437293db3d746ada76de7261173801c0c

Fix help textbox display for skills.

src/text.c
@@ -3019,7 +3019,7 @@ const char* const texts[] = {
 #include "texts.txt"
 };
 
-extern int lastTextID;
+extern const char * lastText;
 extern char decodedText[];
 extern const char * compressedText[];
 
@@ -3064,12 +3064,8 @@ void stripTextControlCode(char *textIn, char *textOut, int maxLength)
 
 char *decodeText(int textID)
 {
-    if(textID == lastTextID && textID != TEXT_SPECIAL_SKILL_HELP && textID != TEXT_ASSIST_SKILL_NAME_IN_ACTION_MENU && textID != TEXT_ASSIST_SKILL_HELP_IN_ACTION_MENU && textID != TEXT_ASSIST_SKILL_HELP_IN_STAT_SCREEN && textID != TEXT_PASSIVE_SKILL_A_HELP && textID != TEXT_PASSIVE_SKILL_B_HELP && textID != TEXT_PASSIVE_SKILL_C_HELP && textID != TEXT_PASSIVE_SKILL_S_HELP && textID != TEXT_NEW_PASSIVE_SKILL_UNLOCKED)
-        return decodedText;
-
-    lastTextID = textID;
-
     char *p = texts[textID];
+
     if(textID == TEXT_SPECIAL_SKILL_HELP)
         p = getSpecialSkillDescriptionText();
     if(textID == TEXT_ASSIST_SKILL_NAME_IN_ACTION_MENU)
@@ -3088,6 +3084,11 @@ char *decodeText(int textID)
         p = getPassiveSkillSHelpText();
     if(textID == TEXT_NEW_PASSIVE_SKILL_UNLOCKED)
         p = getNewUnlockedPassiveSkillNameTextByCurrentAIS();
+    
+    if(p == lastText)
+        return decodedText;
+    
+    lastText = p;
 
     char *p0 = p;
     char *q = decodedText;

linkerscript
@@ -76,7 +76,7 @@ gBattleTarget = 0x203a46c;
 gBattleHitArray = 0x203a4ec;
 
 decodedText = 0x202A5B0;
-lastTextID = 0x202B5B0;
+lastText = 0x202B5B0;
 
 ReadSramFast = 0x3005d90;
 

commit 9d4a091da963900c0b05385cb149d4dd1bef203f

Fix text display bug introduced in https://github.com/laqieer/FEHR/commit/fa27da8437293db3d746ada76de7261173801c0c.

src/text.c
@@ -14,6 +14,7 @@
  * */
 
 #include <stdbool.h>
+#include <stddef.h>
 
 #include "text.h"
 #include "text_id.h"
@@ -3084,6 +3085,18 @@ char *decodeText(int textID)
         p = getPassiveSkillSHelpText();
     if(textID == TEXT_NEW_PASSIVE_SKILL_UNLOCKED)
         p = getNewUnlockedPassiveSkillNameTextByCurrentAIS();
+
+    if(p == NULL)
+    {
+        p = compressedText[textID];
+
+        if(p != lastText)
+            decompressText(p, decodedText);
+
+        lastText = p;
+
+        return decodedText;
+    }
     
     if(p == lastText)
         return decodedText;
@@ -3092,47 +3105,43 @@ char *decodeText(int textID)
 
     char *p0 = p;
     char *q = decodedText;
-    if(textID < sizeof(texts) / 4 && p)
-    {
-        unsigned int maxLineWidth = TEXT_LINE_WIDTH_MAX;
-        if(textID == TEXT_SPECIAL_SKILL_HELP || textID == TEXT_ASSIST_SKILL_HELP_IN_ACTION_MENU || textID == TEXT_ASSIST_SKILL_HELP_IN_STAT_SCREEN || textID == TEXT_PASSIVE_SKILL_A_HELP || textID == TEXT_PASSIVE_SKILL_B_HELP || textID == TEXT_PASSIVE_SKILL_C_HELP || textID == TEXT_PASSIVE_SKILL_S_HELP)
-            maxLineWidth = SKILL_HELP_WIDTH_MAX;
+    unsigned int maxLineWidth = TEXT_LINE_WIDTH_MAX;
+    if(textID == TEXT_SPECIAL_SKILL_HELP || textID == TEXT_ASSIST_SKILL_HELP_IN_ACTION_MENU || textID == TEXT_ASSIST_SKILL_HELP_IN_STAT_SCREEN || textID == TEXT_PASSIVE_SKILL_A_HELP || textID == TEXT_PASSIVE_SKILL_B_HELP || textID == TEXT_PASSIVE_SKILL_C_HELP || textID == TEXT_PASSIVE_SKILL_S_HELP)
+        maxLineWidth = SKILL_HELP_WIDTH_MAX;
 
-        // copy text directly
-        if (getStringTextWidth(p) <= maxLineWidth)
-            while (*p)
+    // copy text directly
+    if (getStringTextWidth(p) <= maxLineWidth)
+        while (*p)
+            *q++ = *p++;
+    else {
+        // add new lines automatically
+        unsigned int charWidth = 0;
+        unsigned int lineWidth = 0;
+
+        while (*p) {
+            if (*p > 0 && *p < 0x20)
                 *q++ = *p++;
-        else {
-            // add new lines automatically
-            unsigned int charWidth = 0;
-            unsigned int lineWidth = 0;
-
-            while (*p) {
-                if (*p > 0 && *p < 0x20)
-                    *q++ = *p++;
-                else {
-                    char *p_next = getCharTextWidth(p, &charWidth);
-                    lineWidth += charWidth;
-                    if (lineWidth > maxLineWidth) {
-                        *q++ = 1; // new line
-                        lineWidth = charWidth;
-                    }
-                    while (*p && p < p_next)
-                        *q++ = *p++;
+            else {
+                char *p_next = getCharTextWidth(p, &charWidth);
+                lineWidth += charWidth;
+                if (lineWidth > maxLineWidth) {
+                    *q++ = 1; // new line
+                    lineWidth = charWidth;
                 }
+                while (*p && p < p_next)
+                    *q++ = *p++;
             }
         }
-        
-        // add 0 to end string
-        *q = 0;
-
-        if(getStringTextWidth(p0) > maxLineWidth)
-        {
-            Debugf("AutoNewLine for text 0x%x, width %d -> %d", textID, getStringTextWidth(p0), getStringTextWidth(decodedText));
-        }
     }
-    else
-        decompressText(compressedText[textID], decodedText);
+    
+    // add 0 to end string
+    *q = 0;
+
+    if(getStringTextWidth(p0) > maxLineWidth)
+    {
+        Debugf("AutoNewLine for text 0x%x, width %d -> %d", textID, getStringTextWidth(p0), getStringTextWidth(decodedText));
+    }
+    
     return decodedText;
 }
 