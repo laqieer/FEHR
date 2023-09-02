89aff4b4de1af4f7e9984698b78fb1bf0d032522
laqieer <laqieer@126.com>
2023-09-03 00:31:39 +0800

    Remove csv data for CromarBot thanks to https://github.com/knabepicer/CromarBot/pull/6/commits/2c4cdbbd118ba4c3e507d7f430eb019df2f55e77.

--- doc/bot/CromarBot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-csv='fehr unit.csv'
-
-cat ../../include/character.h  | head -n 213 | tail -n +24 | grep ';' | sed 's/struct//g' | awk '{print $2}' | tr ';' ',' | tr -d '\n' > $csv
-echo 'comment' >> $csv
-grep ' -> ' ../../src/character.c | grep -v '//{' | grep -v Generic | grep -v Enemy >> $csv
-
-./check_face.py "$csv"
