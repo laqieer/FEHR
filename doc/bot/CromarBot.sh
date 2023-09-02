e690cf9d88aaaa8531462c880025bfb31656bfc4
laqieer <laqieer@126.com>
2023-09-02 20:34:45 +0800

    Add header for last column in unit data csv.

--- doc/bot/CromarBot.sh
+++ doc/bot/CromarBot.sh
@@ -3,7 +3,7 @@
 csv='fehr unit.csv'
 
 cat ../../include/character.h  | head -n 213 | tail -n +24 | grep ';' | sed 's/struct//g' | awk '{print $2}' | tr ';\n' ', ' > $csv
-echo '' >> $csv
+echo 'comment' >> $csv
 grep ' -> ' ../../src/character.c | grep -v '//{' | grep -v Generic | grep -v Enemy >> $csv
 
 ./check_face.py "$csv"
